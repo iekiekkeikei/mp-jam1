629d2d50b3b4624fbfebdd0fd982420048ef5e37
keikei <michikataoka@live.jp>
2025-03-23 18:03:02 +0900

    botaniaとSpell booksの布素材に対してレシピの同化
    botaniaのmanaweave_clothとSpell booksのmagic_clothのレシピ同化

--- overrides/kubejs/server_scripts/recipe_botania_spellbooks.js
+++ overrides/kubejs/server_scripts/recipe_botania_spellbooks.js
@@ -1,6 +1,8 @@
-ServerEvents.tags('item', event => {  
+ServerEvents.tags('item', event => {
     event.add('forge:mana_ingot', 'irons_spellbooks:arcane_ingot')
     event.add('forge:mana_ingot', 'botania:manasteel_ingot')
+    event.add('forge:mana_cloth', 'irons_spellbooks:magic_cloth')
+    event.add('forge:mana_cloth', 'botania:manaweave_cloth')
   })
 ServerEvents.recipes(event => {
     //event.remove({ output: 'irons_spellbooks:arcane_ingot' })
@@ -13,5 +15,15 @@ ServerEvents.recipes(event => {
         { input: 'botania:manasteel_ingot' },
         'botania:manasteel_ingot',
         '#forge:mana_ingot'
+    )
+    event.replaceInput(
+        { input: 'irons_spellbooks:magic_cloth' },
+        'irons_spellbooks:magic_cloth',
+        '#forge:mana_cloth'
+    )
+    event.replaceInput(
+        { input: 'botania:manaweave_cloth' },
+        'botania:manaweave_cloth',
+        '#forge:mana_cloth'
     )
 })
